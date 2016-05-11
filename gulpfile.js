4280823cffc71afc28593f3a350dfa44609a188e
David Neukirchen <david.neukirchen@gmail.com>
2016-05-11 18:13:07 +0200

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,73 @@
+'use strict';
+var gulp = require('gulp');
+var filter = require('gulp-filter');
+var sass = require('gulp-sass');
+var minifyCss = require('gulp-minify-css');
+var sourcemaps = require('gulp-sourcemaps');
+var uglify = require('gulp-uglify');
+var autoprefixer = require('gulp-autoprefixer');
+var concat = require('gulp-concat');
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+var config = {
+    bowerDir: '.bower_components',
+    bootstrapDir: './bower_components/bootstrap-sass',
+    fontAwesomeDir: './bower_components/font-awesome'
+};
+
+/**
+ * CSS
+ */
+gulp.task('css', function () {
+    // Scss config
+    var scssConfig = {
+        advanced: true,
+        aggressiveMerging: true,
+        mediaMerging: true,
+        includePaths: [
+            config.bootstrapDir + '/assets/stylesheets',
+            config.fontAwesomeDir + '/scss'
+        ]
+    };
+
+    return gulp.src('./src/styles/template.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass(scssConfig).on('error', sass.logError))
+        .pipe(autoprefixer())
+        .pipe(minifyCss())
+        .pipe(concat('template.min.css'))
+        .pipe(sourcemaps.write('../maps'))
+        .pipe(gulp.dest('./src/css/'));
+});
+
+/**
+ * JS
+ */
+gulp.task('js', function () {
+    return gulp.src([
+            // config.bootstrapDir + '/assets/javascripts/bootstrap/collapse.js',
+            // config.bootstrapDir + '/assets/javascripts/bootstrap/dropdown.js',
+            // config.bootstrapDir + '/assets/javascripts/bootstrap/modal.js',
+            config.bootstrapDir + '/assets/javascripts/bootstrap/transition.js',
+            './src/js/template.js',
+        ])
+        .pipe(sourcemaps.init())
+        .pipe(uglify())
+        .pipe(concat('template.min.js'))
+        .pipe(sourcemaps.write('../maps'))
+        .pipe(gulp.dest('./src/js/'));
+});
+
+gulp.task('icons', function () {
+    return gulp.src(config.fontAwesomeDir + '/fonts/**.*')
+        .pipe(gulp.dest('./src/fonts'));
+});
+
+/**
+ * Default Watcher
+ */
+gulp.task('default', ['css', 'js'], function () {
+    gulp.watch(['./src/scss/**/*.scss'], ['css']);
+    gulp.watch(['./src/js/**/*.js'], ['js']);
+});
