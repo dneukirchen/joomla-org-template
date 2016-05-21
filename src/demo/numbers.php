fd8a5fe74ae778b227da7292b19d476e1d0e7df6
David Neukirchen <david.neukirchen@gmail.com>
2016-05-22 01:37:24 +0200

    WIP numbers section

--- src/demo/numbers.php
+++ src/demo/numbers.php
@@ -1,24 +1,77 @@
-<div class="col-md-2">
-
+<div class="col-md-2 numbers-box installations">
+	<div class="numbers-text">
+		<span>65</span><sup>+</sup><br/>
+		Million<br/>
+		Installations
+	</div>
 </div>
-<div class="col-md-2">
-
+<div class="col-md-2 numbers-box installations">
+	<div class="numbers-overlay">
+		<div class="numbers-img"></div>
+	</div>
 </div>
-<div class="col-md-2">
 
+<div class="col-md-2 numbers-box extensions">
+	<div class="numbers-text">
+		<span>13</span><sup>+</sup><br/>
+		Thousand<br/>
+		Extensions &<br/>
+		Templates
+	</div>
 </div>
-<div class="col-md-2">
-
+<div class="col-md-2 numbers-box extensions">
+	<div class="numbers-overlay">
+		<div class="numbers-img"></div>
+	</div>
 </div>
-<div class="col-md-2">
 
+<div class="col-md-2 numbers-box support">
+	<div class="numbers-text">
+		<span>3</span><sup>+</sup><br/>
+		Million<br/>
+		Forum Posts
+	</div>
 </div>
-<div class="col-md-2">
-
+<div class="col-md-2 numbers-box support">
+	<div class="numbers-overlay">
+		<div class="numbers-img"></div>
+	</div>
 </div>
-<div class="col-md-2">
 
+<div class="col-md-2 numbers-box volunteers">
+	<div class="numbers-overlay">
+		<div class="numbers-img"></div>
+	</div>
 </div>
-<div class="col-md-2">
+<div class="col-md-2 numbers-box volunteers">
+	<div class="numbers-text">
+		<span>1</span><sup>+</sup><br/>
+		Thousands<br/>
+		Volunteers
+	</div>
+</div>
 
+<div class="col-md-2 numbers-box development">
+	<div class="numbers-overlay">
+		<div class="numbers-img"></div>
+	</div>
+</div>
+<div class="col-md-2 numbers-box development">
+	<div class="numbers-text">
+		<span>550</span><sup>+</sup><br/>
+		Thousands<br/>
+		Lines of Code
+	</div>
+</div>
+
+<div class="col-md-2 numbers-box languages">
+	<div class="numbers-overlay">
+		<div class="numbers-img"></div>
+	</div>
+</div>
+<div class="col-md-2 numbers-box languages">
+	<div class="numbers-text">
+		<span>64</span><br/>
+		Languages
+	</div>
 </div>
