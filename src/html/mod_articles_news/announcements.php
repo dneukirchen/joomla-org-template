8b434d4630e0086828a10fe846bee783e8dd07d8
David Neukirchen <david.neukirchen@gmail.com>
2016-05-22 14:06:47 +0200

    Announcements fine tuning

--- src/html/mod_articles_news/announcements.php
+++ src/html/mod_articles_news/announcements.php
@@ -3,4 +3,5 @@
 	<?php foreach ($list as $item) : ?>
 		<?php require JModuleHelper::getLayoutPath('mod_articles_news', 'announcements_item'); ?>
 	<?php endforeach; ?>
+	<a class="read-all" href="https://www.joomla.org/announcements.html">Read all <span class="fa fa-angle-right"></span></a>
 </div>
