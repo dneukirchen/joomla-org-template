69f93aa93ec3c28b1b01a1384ea44e51040fcfb7
David Neukirchen <david.neukirchen@gmail.com>
2016-05-29 13:01:43 +0200

    Add why-joomla responsive styling

--- src/demo/why-joomla.php
+++ src/demo/why-joomla.php
@@ -1,5 +1,11 @@
 <?php defined('_JEXEC') or die; ?>
-<div class="col-md-6">
+<div class="col-md-6 col-md-push-6">
+	<div class="embed-responsive embed-responsive-16by9">
+		<iframe src="https://www.youtube.com/embed/HMRM7CTnFAA?rel=0&controls=1&theme=light&showinfo=0"
+		        width="560" height="315" frameborder="0" allowfullscreen="allowfullscreen"></iframe>
+	</div>
+</div>
+<div class="col-md-6 col-md-pull-6">
 	<h2 class="text-default">Why Joomla?</h2>
 	<p>Hundreds of developers have been improving Joomla since the first version was released in 2005.
 		This immense effort has made Joomla very popular, easy to use, stable and secure.</p>
@@ -8,10 +14,4 @@
 	<p>
 		<a href="#" class="btn btn-danger">Read more about Joomla!</a>
 	</p>
-</div>
-<div class="col-md-6">
-	<div class="embed-responsive embed-responsive-16by9">
-		<iframe src="https://www.youtube.com/embed/HMRM7CTnFAA?rel=0&controls=1&theme=light&showinfo=0"
-		        width="560" height="315" frameborder="0" allowfullscreen="allowfullscreen"></iframe>
-	</div>
 </div>
