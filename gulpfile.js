557cb6f5d41b00271bfb8379db34107dda13fbe1
David Neukirchen <david.neukirchen@gmail.com>
2016-05-29 18:12:53 +0200

    Add jquery waypoints

--- gulpfile.js
+++ gulpfile.js
@@ -43,6 +43,7 @@ gulp.task('js', function () {
             config.bowerDir + '/bootstrap-sass/assets/javascripts/bootstrap/collapse.js',
             config.bowerDir + '/bootstrap-sass/assets/javascripts/bootstrap/dropdown.js',
             config.bowerDir + '/bootstrap-sass/assets/javascripts/bootstrap/transition.js',
+            config.bowerDir + '/waypoints/lob/jquery.waypoints.js',
             './src/js/template.js',
         ])
         .pipe(uglify())
