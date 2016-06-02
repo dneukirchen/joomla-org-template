b6f4c50136de096949fcd4bd7f435e5ac4273180
David Neukirchen <david.neukirchen@gmail.com>
2016-06-02 20:02:15 +0200

    Add rochen logo

--- src/demo/footer-info.php
+++ src/demo/footer-info.php
@@ -2,7 +2,7 @@
 <div class="col-sm-6">
 	<p>
 		<a href="<?php echo $this->baseurl; ?>" title="Joomla Frontpage">
-			<img src="<?php echo $templatePath . '/img/joomla-logo.png'; ?>" alt="Joomla Logo"/>
+			<img class="joomla-logo" src="<?php echo $templatePath . '/img/joomla-logo.png'; ?>" alt="Joomla Logo"/>
 		</a>
 		<span>2005-<?php date('Y'); ?> Open Source Matters, Inc. All Rights Reserved</span></p>
 </div>
@@ -10,6 +10,6 @@
 	<ul>
 		<li><a href="https://www.joomla.org/accessibility-statement.html">Accessibility Statement</a></li>
 		<li><a href="https://www.joomla.org/privacy-policy.html">Privacy Policy</a></li>
-		<li><a href="https://www.rochen.com/joomla-hosting" target="_blank">Joomla Hosting by Rochen</a></li>
+		<li><a href="https://www.rochen.com/joomla-hosting" target="_blank">Joomla Hosting by <img class="rochen-logo" src="<?php echo $templatePath; ?>/img/rochen.png" /></a></li>
 	</ul>
 </div>
