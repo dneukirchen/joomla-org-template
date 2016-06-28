d312aba74085017a492aa04256bf6268d6ad1d8a
David Neukirchen <david.neukirchen@gmail.com>
2016-06-28 18:36:26 +0200

    Add social media svg icons

--- src/demo/social-media.php
+++ src/demo/social-media.php
@@ -2,7 +2,7 @@
 	<li>
 		<a href="https://twitter.com/joomla" target="_blank" title="Follow Us on Twitter">
 			<span class="icon-circled icon-circled-sm icon-circled-white">
-				<span class="fa fa-twitter"></span>
+				<?php echo HtmlHelper::svgIcon('twitter'); ?>
 			</span>
 			<span class="social-media-title">Twitter</span>
 		</a>
@@ -10,7 +10,7 @@
 	<li>
 		<a href="https://www.facebook.com/joomla" target="_blank" title="Joomla! on Facebook">
 			<span class="icon-circled icon-circled-sm icon-circled-white">
-				<span class="fa fa-facebook"></span>
+				<?php echo HtmlHelper::svgIcon('facebook'); ?>
 			</span>
 			<span class="social-media-title">Facebook</span>
 		</a>
@@ -18,7 +18,7 @@
 	<li>
 		<a href="https://plus.google.com/+joomla/posts" target="_blank" title="Joomla! on Google+">
 			<span class="icon-circled icon-circled-sm icon-circled-white">
-				<span class="fa fa-google"></span>
+				<?php echo HtmlHelper::svgIcon('google-plus'); ?>
 			</span>
 			<span class="social-media-title">Google+</span>
 		</a>
@@ -26,7 +26,7 @@
 	<li>
 		<a href="https://www.youtube.com/user/joomla" target="_blank" title="Joomla's YouTube Channel">
 			<span class="icon-circled icon-circled-sm icon-circled-white">
-				<span class="fa fa-youtube"></span>
+				<?php echo HtmlHelper::svgIcon('youtube'); ?>
 			</span>
 			<span class="social-media-title">YouTube</span>
 		</a>
@@ -34,21 +34,21 @@
 	<li>
 		<a href="https://www.linkedin.com/company/joomla" target="_blank" title="Joomla! on Linkedin">
 			<span class="icon-circled icon-circled-sm icon-circled-white">
-				<span class="fa fa-linkedin"></span>
+				<?php echo HtmlHelper::svgIcon('linkedin'); ?>
 			</span>
 			<span class="social-media-title">Linkedin</span></a>
 	</li>
 	<li>
 		<a href="https://www.pinterest.com/joomla" target="_blank" title="Joomla's Pinterest Board">
 			<span class="icon-circled icon-circled-sm icon-circled-white">
-				<span class="fa fa-pinterest"></span>
+				<?php echo HtmlHelper::svgIcon('pinterest'); ?>
 			</span>
 			<span class="social-media-title">Pinterest</span></a>
 	</li>
 	<li>
 		<a href="https://github.com/joomla" target="_blank" title="Joomla's GitHub">
 			<span class="icon-circled icon-circled-sm icon-circled-white">
-				<span class="fa fa-github"></span>
+				<?php echo HtmlHelper::svgIcon('github'); ?>
 			</span>
 			<span class="social-media-title">GitHub</span>
 		</a>
