cc33eb2bf25cc1aaf869cd71c912ac97b7dfff66
David Neukirchen <david.neukirchen@gmail.com>
2016-06-28 19:43:54 +0200

    Responsive tuning

--- src/demo/numbers.php
+++ src/demo/numbers.php
@@ -1,5 +1,5 @@
 <?php defined('_JEXEC') or die; ?>
-<div class="col-xs-4 col-md-2 numbers-box installations">
+<div class="col-xs-6 col-sm-4 col-md-2 numbers-box installations">
 	<div class="numbers-text">
 		<span class="number"><span class="count-up" data-from="0" data-to="65">0</span> <sup>+</sup></span><br/>Million<br/>Installations
 	</div>
@@ -10,7 +10,7 @@
 	</div>
 </div>
 
-<div class="col-xs-4 col-md-2 numbers-box extensions">
+<div class="col-xs-6 col-sm-4 col-md-2 numbers-box extensions">
 	<div class="numbers-text">
 		<span class="number"><span class="count-up" data-from="0" data-to="13">0</span> <sup>+</sup></span><br/>Thousand<br/>Extensions &<br/>Templates
 	</div>
@@ -21,7 +21,7 @@
 	</div>
 </div>
 
-<div class="col-xs-4 col-md-2 numbers-box support">
+<div class="col-xs-6 col-sm-4 col-md-2 numbers-box support">
 	<div class="numbers-text">
 		<span class="number"><span class="count-up" data-from="0" data-to="3">0</span> <sup>+</sup></span><br/>Million<br/>Forum Posts
 	</div>
@@ -37,7 +37,7 @@
 		<div class="numbers-overlay"></div>
 	</div>
 </div>
-<div class="col-xs-4 col-md-2 numbers-box volunteers">
+<div class="col-xs-6 col-sm-4 col-md-2 numbers-box volunteers">
 	<div class="numbers-text">
 		<span class="number"><span class="count-up" data-from="0" data-to="1" data-speed="1000">0</span> <sup>+</sup></span><br/>Thousand<br/>Volunteers
 	</div>
@@ -48,7 +48,7 @@
 		<div class="numbers-overlay"></div>
 	</div>
 </div>
-<div class="col-xs-4 col-md-2 numbers-box development">
+<div class="col-xs-6 col-sm-4 col-md-2 numbers-box development">
 	<div class="numbers-text">
 		<span class="number"><span class="count-up" data-from="0" data-to="550">0</span> <sup>+</sup></span><br/>Thousands<br/>Lines of Code
 	</div>
@@ -59,7 +59,7 @@
 		<div class="numbers-overlay"></div>
 	</div>
 </div>
-<div class="col-xs-4 col-md-2 numbers-box languages">
+<div class="col-xs-6 col-sm-4 col-md-2 numbers-box languages">
 	<div class="numbers-text">
 		<span class="number"><span class="count-up" data-from="0" data-to="64">0</span></span><br/>Languages
 	</div>
--- src/demo/who-is-using.php
+++ src/demo/who-is-using.php
@@ -6,10 +6,12 @@ $logos = glob(__DIR__ .'/img/using-joomla/*.{jpg,png}', GLOB_BRACE);
 	<p>Joomla is trusted by some of the world’s most well-known companies and much-loved brands like Peugeot, Maersk, Harvard University and even websites for MTV and Gorillaz!</p>
 </div>
 
-<div class="row">
-	<?php foreach($logos as $logo) : ?>
-		<div class="col-xs-6 col-md-3">
-			<div class="logo" style="background-image: url('<?php echo $templatePath . '/demo/img/using-joomla/' . basename($logo); ?>')"></div>
-		</div>
-	<?php endforeach; ?>
+<div class="col-xs-12">
+	<div class="row">
+		<?php foreach($logos as $logo) : ?>
+			<div class="col-sm-6 col-md-3">
+				<div class="logo" style="background-image: url('<?php echo $templatePath . '/demo/img/using-joomla/' . basename($logo); ?>')"></div>
+			</div>
+		<?php endforeach; ?>
+	</div>
 </div>
