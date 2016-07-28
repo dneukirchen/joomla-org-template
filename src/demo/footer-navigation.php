627a9e4a208d8f9f286897f5dcd76ac4275332e8
David Neukirchen <david.neukirchen@gmail.com>
2016-07-28 17:57:21 +0200

    Add footer links, resolves #24. thx @arunsasi

--- src/demo/footer-navigation.php
+++ src/demo/footer-navigation.php
@@ -1,47 +1,45 @@
 <?php defined('_JEXEC') or die; ?>
-
-
 <div class="col-sm-6 col-md-3">
 	<h4 class="text-primary">Get Started</h4>
 	<ul>
-		<li><a href="#">Launch on Joomla.com</a></li>
-		<li><a href="#">Joomla! Demo</a></li>
-		<li><a href="#">Download Joomla!</a></li>
-		<li><a href="#">Documentation</a></li>
-		<li><a href="#">Joomla! Extensions</a></li>
-		<li><a href="#">Find a Professional</a></li>
-		<li><a href="#">Community Forum</a></li>
+		<li><a href="http://www.joomla.com/">Launch on Joomla.com</a></li>
+		<li><a href="https://demo.joomla.org/">Joomla! Demo</a></li>
+		<li><a href="https://www.joomla.org/download.html">Download Joomla!</a></li>
+		<li><a href="https://docs.joomla.org/">Documentation</a></li>
+		<li><a href="http://extensions.joomla.org/">Joomla! Extensions</a></li>
+		<li><a href="http://resources.joomla.org/en/">Find a Professional</a></li>
+		<li><a href="http://forum.joomla.org/">Community Forum</a></li>
 	</ul>
 </div>
 <div class="col-sm-6 col-md-3">
 	<h4 class="text-secondary-green">Get Technical</h4>
 	<ul>
-		<li><a href="#">Developer Documentation</a></li>
-		<li><a href="#">Developer Network</a></li>
-		<li><a href="#">Report an Issue</a></li>
-		<li><a href="#">Bug Squad</a></li>
-		<li><a href="#">GitHub</a></li>
+		<li><a href="https://docs.joomla.org/">Developer Documentation</a></li>
+		<li><a href="https://developer.joomla.org/">Developer Network</a></li>
+		<li><a href="https://issues.joomla.org/">Report an Issue</a></li>
+		<li><a href="https://docs.joomla.org/Bug_Squad">Bug Squad</a></li>
+		<li><a href="https://github.com/Joomla">GitHub</a></li>
 	</ul>
 </div>
 <div class="clearfix visible-sm-block"></div>
 <div class="col-sm-6 col-md-3">
 	<h4 class="text-secondary-yellow">Get Involved</h4>
 	<ul>
-		<li><a href="#">Community Portal</a></li>
-		<li><a href="#">Community Forum</a></li>
-		<li><a href="#">Upcoming Events</a></li>
-		<li><a href="#">Local Usergroups</a></li>
-		<li><a href="#">Volunteer</a></li>
+		<li><a href="https://community.joomla.org/">Community Portal</a></li>
+		<li><a href="http://forum.joomla.org/">Community Forum</a></li>
+		<li><a href="https://events.joomla.org/">Upcoming Events</a></li>
+		<li><a href="https://community.joomla.org/user-groups.html">Local Usergroups</a></li>
+		<li><a href="https://volunteers.joomla.org/">Volunteer</a></li>
 	</ul>
 </div>
 <div class="col-sm-6 col-md-3">
 	<h4 class="text-additional-secondary-orange">Get Information</h4>
 	<ul>
-		<li><a href="#">About Joomla!</a></li>
-		<li><a href="#">Features</a></li>
-		<li><a href="#">Joomla! Magazine</a></li>
-		<li><a href="#">Announcements</a></li>
-		<li><a href="#">Shop</a></li>
-		<li><a href="#">Contact</a></li>
+		<li><a href="https://www.joomla.org/about-joomla.html">About Joomla!</a></li>
+		<li><a href="https://www.joomla.org/core-features.html">Features</a></li>
+		<li><a href="http://magazine.joomla.org/">Joomla Magazine</a></li>
+		<li><a href="https://www.joomla.org/announcements.html">Announcements</a></li>
+		<li><a href="https://shop.joomla.org/">Shop</a></li>
+		<li><a href="https://www.joomla.org/media-contact.html">Contact</a></li>
 	</ul>
 </div>
