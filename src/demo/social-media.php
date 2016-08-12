1aa2c9643e6394ce599e2c586b8c2f77c247558e
Soren <soren@notwebdesign.com>
2016-08-12 13:56:45 +0200

    Added hidden-sm to social media labels

--- src/demo/social-media.php
+++ src/demo/social-media.php
@@ -4,7 +4,7 @@
 			<span class="icon-circled icon-circled-sm icon-circled-white">
 				<?php echo HtmlHelper::svgIcon('twitter'); ?>
 			</span>
-			<span class="social-media-title">Twitter</span>
+			<span class="social-media-title hidden-sm">Twitter</span>
 		</a>
 	</li>
 	<li>
@@ -12,7 +12,7 @@
 			<span class="icon-circled icon-circled-sm icon-circled-white">
 				<?php echo HtmlHelper::svgIcon('facebook'); ?>
 			</span>
-			<span class="social-media-title">Facebook</span>
+			<span class="social-media-title hidden-sm">Facebook</span>
 		</a>
 	</li>
 	<li>
@@ -20,7 +20,7 @@
 			<span class="icon-circled icon-circled-sm icon-circled-white">
 				<?php echo HtmlHelper::svgIcon('google-plus'); ?>
 			</span>
-			<span class="social-media-title">Google+</span>
+			<span class="social-media-title hidden-sm">Google+</span>
 		</a>
 	</li>
 	<li>
@@ -28,7 +28,7 @@
 			<span class="icon-circled icon-circled-sm icon-circled-white">
 				<?php echo HtmlHelper::svgIcon('youtube'); ?>
 			</span>
-			<span class="social-media-title">YouTube</span>
+			<span class="social-media-title hidden-sm">YouTube</span>
 		</a>
 	</li>
 	<li>
@@ -36,21 +36,21 @@
 			<span class="icon-circled icon-circled-sm icon-circled-white">
 				<?php echo HtmlHelper::svgIcon('linkedin'); ?>
 			</span>
-			<span class="social-media-title">Linkedin</span></a>
+			<span class="social-media-title hidden-sm">Linkedin</span></a>
 	</li>
 	<li>
 		<a href="https://www.pinterest.com/joomla" target="_blank" title="Joomla's Pinterest Board">
 			<span class="icon-circled icon-circled-sm icon-circled-white">
 				<?php echo HtmlHelper::svgIcon('pinterest'); ?>
 			</span>
-			<span class="social-media-title">Pinterest</span></a>
+			<span class="social-media-title hidden-sm">Pinterest</span></a>
 	</li>
 	<li>
 		<a href="https://github.com/joomla" target="_blank" title="Joomla's GitHub">
 			<span class="icon-circled icon-circled-sm icon-circled-white">
 				<?php echo HtmlHelper::svgIcon('github'); ?>
 			</span>
-			<span class="social-media-title">GitHub</span>
+			<span class="social-media-title hidden-sm">GitHub</span>
 		</a>
 	</li>
 </ul>
